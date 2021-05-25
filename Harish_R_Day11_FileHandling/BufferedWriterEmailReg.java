bd20e9c9bc14aab4f73ac9957794d52bae4c0bb0
Harish-HRX <57002225+Harish-HRX@users.noreply.github.com>
2021-05-25 14:13:56 +0530

    FileHandling Programs

---
+++ Harish_R_Day11_FileHandling/BufferedWriterEmailReg.java
@@ -0,0 +1,23 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class BufferedWriterEmailReg {
+		
+	public static void main(String[] args) {
+		try {
+			Scanner in=new Scanner(System.in);
+			String name=in.nextLine();
+			String id=in.nextLine();
+			FileWriter fw=new FileWriter("C:\\Users\\Haris\\Desktop\\webDev\\test.txt");				BufferedWriter bw=new BufferedWriter(fw);
+				bw.write("Welcome "+name+" thank you for registering your email id"+id);
+				bw.flush();
+				bw.close();
+			}
+			catch(IOException o) {
+				o.printStackTrace();;
+			}
+	}
+
+}
